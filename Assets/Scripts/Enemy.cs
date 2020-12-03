0173b7a9d76d29498ae83aed89f4a548dc2f0ba0
Ann-Louise <boznauk@hotmail.com>
2020-12-03 13:09:13 +0100

    New enemy scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,13 +8,13 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] float health = 100f;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{ 
+    private Transform Player;
+    [SerializeField] int MoveSpeed = 4;
+    [SerializeField] int MaxDist = 10;
+    [SerializeField] int MinDist = 5;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            Debug.Log("Looking at player");
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Here call any function you want, such as Shoot at player or something
+            }
+
+        }
+    }
+}
+
+
