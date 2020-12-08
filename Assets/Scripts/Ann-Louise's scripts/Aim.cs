69e7446779ad31ba0cdabb903523352c9d821b62
Smile9937 <smile9937@gmail.com>
2020-12-08 13:16:52 +0100

    Added Aim script on the weapon, removed from player. No more spinny player, just spinny weapon!

---
+++ Assets/Scripts/Ann-Louise's scripts/Aim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+     // Update is called once per frame
+    void Update()
+    {
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10)) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10 * Time.deltaTime);
+    }
+
+}
